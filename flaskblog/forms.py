3785c4cf4848c276a6c5bf4ca6db18090c4f3372
Nguyễn Ngọc Anh Tuấn <nnatuan2003@gmail.com>
2024-01-01 12:44:27 +0700

    update create,up and delete post

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField,FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError, TextAreaField
 from wtforms.validators import DataRequired,Length,Email,EqualTo
 from flaskblog.models import User
 
@@ -54,3 +54,7 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError('That email is taken, please choose another one')
             
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
